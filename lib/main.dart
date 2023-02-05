a23a7a430999eb4ca1991fa8f667e9f6dd7dccfc
Ruan_Hanekom <rhanekombiz12@gmail.com>
2023-02-05 17:42:34 +0000

    Initial Commit with transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'transactions.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  runApp(const PersonalExpenseTracker());
+}
+
+class PersonalExpenseTracker extends StatelessWidget {
+  const PersonalExpenseTracker({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Personal Expense Tracker',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New shoes', amount: 950.00, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Groceries', amount: 110.00, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.deepPurpleAccent,
+        title: const Text('Track Your Expenses!'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                color: Colors.deepPurple,
+                child: Text('CHART'),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  elevation: 5,
+                  color: Colors.deepPurple,
+                  child: Row(children: <Widget>[
+                    Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'R: ' + tx.amount.toStringAsFixed(2),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        )),
+                    Column(children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ]),
+                  ]),
+                );
+              }).toList(),
+            ),
+          ]),
     );
   }
 }
