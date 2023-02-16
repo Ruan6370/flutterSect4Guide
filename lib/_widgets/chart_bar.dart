b81858b2bce2ada4c3c73746799300926f855902
Squishicus_Maximus <rhanekombiz12@gmail.com>
2023-02-16 19:32:14 +0000

    Improved Chart

--- lib/_widgets/chart_bar.dart
+++ lib/_widgets/chart_bar.dart
@@ -13,7 +13,10 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('R ' + spendingAmount.toStringAsFixed(0))),
+        Container(
+            height: 20,
+            child: FittedBox(
+                child: Text('R ' + spendingAmount.toStringAsFixed(0)))),
         const SizedBox(
           height: 4,
         ),
